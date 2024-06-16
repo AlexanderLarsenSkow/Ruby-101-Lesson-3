15868deb6f368185eef4082dcc18d47ab6024c5c
Alex Skow <askow2015@gmail.com>
2024-06-16 22:20:38 +0000

    Finished Exercise 5

---
+++ easy2_exercises/5.rb
@@ -0,0 +1,7 @@
+# Show an easier way to write this array.
+
+flintstones = ["Fred", "Barney", "Wilma", "Betty", "BamBam", "Pebbles"]
+
+flintstones2 = %w(Fred Barney Wilma Betty BamBam Pebbles)
+
+p flintstones2
